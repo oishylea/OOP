241201dff61e8365639dceb46646d7478663b119
Aliatul Izzah Jasman <izzahalia6@gmail.com>
2024-09-03 03:49:38 +0800

    Add files via upload

---
+++ Object Oriented Programming/MyFirstJava/App.java
@@ -0,0 +1,10 @@
+package MyFirstJava;
+
+//The main class name must be same as the filename before .java
+public class App{
+
+    public static void main (String[] args) throws Exception{
+        System.out.println("Hello,World!");
+        System.out.println("Test");
+    }
+}
---
+++ Object Oriented Programming/SECONDAPP/App/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args)
+    {
+        int age=20;
+
+        System.out.println("Umur saya : "+age+" tahun");
+
+        age=age+5;
+
+        System.out.println("5 tahun lagi umur saya : "+age+" tahun");
+
+    }
+}
